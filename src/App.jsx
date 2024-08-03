eaf9490d553634f8a33712a821c7ba197147f799
NERibeiro <renanpvh@hotmail.com>
2024-08-03 01:22:13 -0400

    alter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 
-import { Title } from "./styles"
+import { Button, Container, ContainerInputs, Form, Input, InputLabel, Title, TopBackground } from "./styles"
 
-function App() {
+function Home() {
 
   return (
-    
-      <div>
 
-<Title>Por enquanto tudo ok</Title>
+    <Container>
 
-  </div>
+      <TopBackground>
+        <img src="" alt="" />
+      </TopBackground>
+
+      <Form>
+        <Title>Cadastro de Usuários</Title>
+
+        <ContainerInputs>
+          <div>
+            <div>
+              <InputLabel>Nome <span>*</span></InputLabel>
+              <Input type="text" placeholder="Nome do Usuário" />
+            </div>
+
+            <div>
+            <InputLabel>Idade <span>*</span></InputLabel>
+            <Input type="number" placeholder="Idade" />
+            </div>
+          </div>
+
+          <div>
+          <InputLabel>E-mail <span>*</span></InputLabel>
+          <Input type="email" placeholder="E-mail do Usuário" />
+          </div>
+
+        </ContainerInputs>
+
+        <Button>Cadastrar Usuário</Button>
+
+
+      </Form>
+
+    </Container>
 
   )
 }
 
-export default App
+export default Home
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,17 @@
 import styled from "styled-components"
 
-export const Title = styled.h2`
-color: red;
-`
+export const Container = styled.div``
+
+export const TopBackground = styled.div``
+
+export const Form = styled.form``
+
+export const ContainerInputs = styled.div``
+
+export const Input = styled.input``
+
+export const InputLabel = styled.label``
+
+export const Button = styled.button``
+
+export const Title = styled.h2``
